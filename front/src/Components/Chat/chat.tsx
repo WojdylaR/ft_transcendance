7d0e842175d5f339bd3c386dc03c938fc0b2a878
Ronan Wojdyla <ronanwojdyla@MacBook-Pro-de-Ronan.local>
2022-06-09 01:48:19 +0200

    push chat debut

--- front/src/Components/Chat/chat.tsx
+++ front/src/Components/Chat/chat.tsx
@@ -1,11 +1,27 @@
-import { useState} from 'react'
+import {useState} from 'react'
+import ChatStyle from '../../Styles/Chat/Chat'
 
 function Chat({src} : any) {
-    const [chat, chatState] = useState("Bienvenue sur le chat")
+    let [chat, chatState] = useState("Bienvenue sur le chat")
+    let [value, chatValue] = useState("Bienvenue sur le chat")
     return (
-        <div>
-            {chat}
-        </div>
+		<div>
+			{chat}
+        	<ChatStyle>
+				<input
+                    onChange={(e) => chatValue(value = e.target.value + '\n')}
+                    onKeyPress={(e) => {
+                                        if(e.key === 'Enter' )
+                                        {
+                                            chatState(chat = chat + '\n' + value)
+                                        }
+                    }}
+                    placeholder="entrez votre message"></input>
+                    <button onClick={(e) => chatState(chat = chat + '\n' + value)}>
+                        Envoyer
+                    </button>
+        	</ChatStyle>
+		</div>
     )
 }
 
